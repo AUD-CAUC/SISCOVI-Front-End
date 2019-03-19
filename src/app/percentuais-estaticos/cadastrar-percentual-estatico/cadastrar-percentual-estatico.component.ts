5b368d8c47c4aa316ac638fe41e5241ee7a4103c
Pdrnunes <48063541+Pdrnunes@users.noreply.github.com>
2019-03-19 14:51:45 -0300

    resolvendo bug de validação de data

--- src/app/percentuais-estaticos/cadastrar-percentual-estatico/cadastrar-percentual-estatico.component.ts
+++ src/app/percentuais-estaticos/cadastrar-percentual-estatico/cadastrar-percentual-estatico.component.ts
@@ -71,10 +71,10 @@ export class CadastrarPercentualEstaticoComponent implements OnInit {
   dataInicioValidator(control: AbstractControl): { [key: string]: any } | null {
     const mensagem = [];
     const otherRegex = new RegExp(/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/);
-
     if (control.parent) {
-      if (this.ultimaData) {
+      if (this.ultimaData[control.parent.get('codigo').value]) {
         if (control.value.toString().length === 10) {
+          // console.log(this.ultimaData[control.parent.get('codigo').value]);
           let dia = 0;
           let mes = 0;
           let ano = 0;
