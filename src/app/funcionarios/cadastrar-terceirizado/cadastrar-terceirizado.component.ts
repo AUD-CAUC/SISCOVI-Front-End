9655e268399e4177f0316c6b4805c11b989bf29a
Pdrnunes <48063541+Pdrnunes@users.noreply.github.com>
2019-03-22 16:52:05 -0300

    Revert "Update cadastrar-terceirizado.component.ts"
    This reverts commit b2055cfedc593f3a3d07d2f8bba0f1f699528d12.

--- src/app/funcionarios/cadastrar-terceirizado/cadastrar-terceirizado.component.ts
+++ src/app/funcionarios/cadastrar-terceirizado/cadastrar-terceirizado.component.ts
@@ -28,7 +28,6 @@ export class CadastrarTerceirizadoComponent implements OnInit {
     cpf: string;
     salvarButtonDisabled = true;
     terceirizadosPlanilhaForm: FormGroup;
-    funcServ: FuncionariosService;
     constructor(private fb: FormBuilder, private  terceirizadoService: FuncionariosService, private  route: ActivatedRoute, private router: Router, private ref: ChangeDetectorRef) {
         this.route.params.subscribe(params => {
           if (!isNaN(params['id'])) {
