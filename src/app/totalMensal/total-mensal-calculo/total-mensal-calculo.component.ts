45832db923270a647c2b6d89283e0fce6cfe3356
Pdrnunes <48063541+Pdrnunes@users.noreply.github.com>
2019-06-03 13:05:21 -0300

    Adicionando o link para gerenciar funções dos terceirizados nas retenções

--- src/app/totalMensal/total-mensal-calculo/total-mensal-calculo.component.ts
+++ src/app/totalMensal/total-mensal-calculo/total-mensal-calculo.component.ts
@@ -6,6 +6,8 @@ import {MaterializeAction} from 'angular2-materialize';
 import {TotalMensalService} from '../total-mensal.service';
 import {TotalMensal} from '../totalMensal';
 import {Mes} from './mes';
+import {ActivatedRoute, Router} from '@angular/router';
+import {RescisaoService} from "../../rescisao/rescisao.service";
 
 @Component({
     templateUrl: './total-mensal-calculo.component.html',
@@ -40,7 +42,7 @@ export class TotalMensalCalculoComponent implements OnInit {
     private mesSelecionado: number;
     private numFuncAtivos: number;
 
-    constructor(contServ: ContratosService, fb: FormBuilder, tmService: TotalMensalService) {
+    constructor(contServ: ContratosService, fb: FormBuilder, tmService: TotalMensalService, private router: Router, private route: ActivatedRoute) {
         this.tmService = tmService;
         this.fb = fb;
         if (contServ.contratos.length === 0) {
@@ -230,4 +232,7 @@ export class TotalMensalCalculoComponent implements OnInit {
         this.closeModal2();
         this.navegaParaViewDeCalculos.emit(this.codigoContrato);
     }
+  goToGerenciarCargos() {
+    this.router.navigate(['./gerenciar-funcoes-terceirizados'], {relativeTo: this.route});
+  }
 }
