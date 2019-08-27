dbde2f7eda4d815313b070114cc0767cfea2f075
Lucas Penido <lpenidoa@me.com>
2019-08-27 12:50:05 -0300

    Criando gráfico na tela inicial.
    Co-Authored-By: pdrnunes <pdrnunes@users.noreply.github.com>

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
-import {ConfigService} from '../_shared/config.service';
+import { Component, AfterViewInit } from '@angular/core';
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: 'inicio.component.html'
 })
-export class InicioComponent {
-  constructor(config: ConfigService) {}
+
+
+export class InicioComponent implements AfterViewInit {
+
+  canvas: any;
+  ctx: any;
+
+  random_rgba() {
+    const o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + r().toFixed(1) + ')';
+  }
+
+  ngAfterViewInit() {
+    this.canvas = document.getElementById('myChart');
+    this.ctx = this.canvas.getContext('2d');
+    const myChart = new Chart(this.ctx, {
+      type: 'pie',
+      data: {
+        labels: ["Eletro", "Capital", "Real"],
+        datasets: [{
+          label: '# of Votes',
+          data: [1,2,3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        legend: {
+          position: 'right',
+        },
+      }
+    });
+  }
+
 }
