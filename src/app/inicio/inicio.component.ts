ba35108c75469e38ced9554f2d59cc2c04a79b1c
Pdrnunes <48063541+Pdrnunes@users.noreply.github.com>
2019-08-30 11:43:17 -0300

    Adicionando 3 tipos de gráfico
    modificando o design dos gráficos de pizza e barra

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -49,38 +49,35 @@ export class InicioComponent implements OnInit {
         this.somaSaldo.push(temp);
       }
     }));
-    this.montaGraficoPizza();
+    this.montaGraficoBarra();
   }
 
   random_rgba() {
     const o = Math.round, r = Math.random, s = 255;
     return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + r().toFixed(1) + ')';
   }
-
+  // Gráfico pizza
   async montaGraficoPizza() {
-    console.log(1)
     const empresas = [];
     this.contratos.map((cont) => {
-      console.log(cont);
       empresas.push(cont.nomeDaEmpresa);
 
     });
-    console.log(empresas);
     await this.demo();
-    this.canvas = document.getElementById('myChart');
+    this.canvas = document.getElementById('myChart2');
     this.ctx = this.canvas.getContext('2d');
     const myChart = new Chart(this.ctx, {
       type: 'pie',
       data: {
         labels: empresas,
         datasets: [{
-          label: '# of Votes',
+          label: 'Número de Funcionários',
           data: this.somaSaldo,
           backgroundColor: [
-            'rgb(228,82,27,1)',
-            'rgb(255,234,106,1)',
-            'rgb(255,124,16, 1)',
-            'rgba(255, 99, 132, 1)',
+            'rgb(40,147,85)',
+            'rgb(255,95,70)',
+            'rgb(255,187,70)',
+            'rgb(116,70,255)',
             'rgba(54, 162, 235, 1)',
             'rgba(255, 206, 86, 1)',
           ],
@@ -88,8 +85,15 @@ export class InicioComponent implements OnInit {
         }]
       },
       options: {
-        legend: {
-          position: 'right',
+        title: {
+          display: true,
+          text: 'Número de Funcionários por Contrato',
+          position: 'left',
+          fontSize: 14,
+          fontFamily: 'roboto'
+        },
+          legend: {
+            position: 'right',
         },
         tooltips: {
           callbacks: {
@@ -109,7 +113,7 @@ export class InicioComponent implements OnInit {
   async demo() {
     await this.sleep(2);
   }
-
+  // Gráfico Barra
   async montaGraficoBarra() {
     // console.log(1)
     // await this.demo()
@@ -120,10 +124,111 @@ export class InicioComponent implements OnInit {
 
     });
     await this.demo();
-    this.canvas = document.getElementById('myChart2');
+    this.canvas = document.getElementById('myChart');
     this.ctx = this.canvas.getContext('2d');
     const myChart = new Chart(this.ctx, {
       type: 'bar',
+      data: {
+        labels: empresas,
+        datasets: [{
+          data: this.somaSaldo,
+          backgroundColor: [
+            'rgb(70,255,163)',
+            'rgb(255,95,70)',
+            'rgb(255,187,70)',
+            'rgb(116,70,255)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        title: {
+          display: true,
+          position: 'top',
+          fontFamily: 'roboto',
+          fontSize: 14,
+          fontcolor: '#000000',
+          text: 'Saldo Acumulado por Contrato'
+        },
+        legend: {
+          display: false,
+          fontFamily: 'roboto'
+        },
+        tooltips: {
+          callbacks: {
+            label: (tooltipItem, data) => {
+              return 'Saldo: R$ ' + data.datasets[0].data[tooltipItem.index].toFixed(2);
+            }
+          }
+        },
+        scales: {
+          yAxes: [{
+            display: true,
+            ticks: {
+              maxTicksLimit: 10,
+              min: 0.0,
+              stepSize: 1000.0,
+            }
+          }]
+        }
+      }
+    });
+  }
+  // Gráfico Radar
+  async montaGraficoRadar() {
+    // console.log(1)
+    // await this.demo()
+    // console.log(2)
+    const empresas = [];
+    this.contratos.map((cont) => {
+      empresas.push(cont.nomeDaEmpresa);
+
+    });
+    await this.demo();
+    this.canvas = document.getElementById('myChart3');
+    this.ctx = this.canvas.getContext('2d');
+    const myChart = new Chart(this.ctx, {
+      type: 'line',
+      data: {
+        labels: empresas,
+        datasets: [{
+          label: '# of Votes',
+          data: this.somaSaldo,
+          backgroundColor: [
+            'rgb(70,255,163)',
+            'rgb(255,95,70)',
+            'rgb(255,187,70)',
+            'rgb(116,70,255)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        legend: {
+          position: 'right',
+        },
+      }
+    });
+  }
+  // Gráfico Linha
+  async montaGraficoLinha() {
+    // console.log(1)
+    // await this.demo()
+    // console.log(2)
+    const empresas = [];
+    this.contratos.map((cont) => {
+      empresas.push(cont.nomeDaEmpresa);
+
+    });
+    await this.demo();
+    this.canvas = document.getElementById('myChart4');
+    this.ctx = this.canvas.getContext('2d');
+    const myChart = new Chart(this.ctx, {
+      type: 'line',
       data: {
         labels: empresas,
         datasets: [{
@@ -148,12 +253,55 @@ export class InicioComponent implements OnInit {
     });
   }
 
+  async montaGraficoPolar() {
+    // console.log(1)
+    // await this.demo()
+    // console.log(2)
+    const empresas = [];
+    this.contratos.map((cont) => {
+      empresas.push(cont.nomeDaEmpresa);
+
+    });
+    await this.demo();
+    this.canvas = document.getElementById('myChart5');
+    this.ctx = this.canvas.getContext('2d');
+    const myChart = new Chart(this.ctx, {
+      type: 'polarArea',
+      data: {
+        labels: empresas,
+        datasets: [{
+          label: '# of Votes',
+          data: this.somaSaldo,
+          backgroundColor: [
+            'rgb(228,82,27,1)',
+            'rgb(255,234,106,1)',
+            'rgb(255,124,16, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        legend: {
+          position: 'right',
+        },
+      }
+    });
+  }
   selecionaGrafico(value: string) {
     this.id = Number(value);
     if (this.id === 1) {
-      this.montaGraficoPizza();
-    } else if (this.id === 2) {
       this.montaGraficoBarra();
+    } else if (this.id === 2) {
+      this.montaGraficoPizza();
+    } else if (this.id === 3) {
+      this.montaGraficoRadar();
+    } else if (this.id === 4) {
+      this.montaGraficoLinha();
+    } else if (this.id === 5) {
+      this.montaGraficoPolar();
     }
   }
 }
