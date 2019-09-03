a1e0ac0d8c0d62d91bd5695179c56d76f5b1b8a1
Lucas Penido <lpenidoa@me.com>
2019-09-03 13:45:51 -0300

    Ajustando gráficos.
    Co-Authored-By: pdrnunes <pdrnunes@users.noreply.github.com>

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,13 +1,14 @@
 import {Component, AfterViewInit, ChangeDetectorRef, Input, OnInit} from '@angular/core';
 import * as Chart from 'chart.js';
-import {ConfigService} from "../_shared/config.service";
-import {SaldoService} from "../saldo/saldo.service";
-import {ContratosService} from "../contratos/contratos.service";
-import {Contrato} from "../contratos/contrato";
-import {SaldoIndividual} from "../saldo/individual/saldo-individual";
+import {ConfigService} from '../_shared/config.service';
+import {SaldoService} from '../saldo/saldo.service';
+import {ContratosService} from '../contratos/contratos.service';
+import {Contrato} from '../contratos/contrato';
+import {SaldoIndividual} from '../saldo/individual/saldo-individual';
 import {TotalMensalService} from '../totalMensal/total-mensal.service';
 import {RubricasService} from '../rubricas/rubricas.service';
 import {Rubrica} from '../rubricas/rubrica';
+import {DecimoTerceiroService} from '../decimo_terceiro/decimo-terceiro.service';
 
 @Component({
   selector: 'app-inicio',
@@ -19,7 +20,7 @@ import {Rubrica} from '../rubricas/rubrica';
 export class InicioComponent implements OnInit {
   @Input() codigoContrato: number;
   contratos: Contrato[];
-  saldos: SaldoIndividual[];
+  saldos: SaldoIndividual[][] = [];
   config: ConfigService;
   somaSaldo = [];
   somaFerias = [];
@@ -35,7 +36,7 @@ export class InicioComponent implements OnInit {
   ctx: any;
 
   constructor(config: ConfigService, private saldoService: SaldoService, private contratoService: ContratosService, private tmService: TotalMensalService, private rubricaService: RubricasService,
-              private ref: ChangeDetectorRef) {
+              private decimoTerceiroService: DecimoTerceiroService, private ref: ChangeDetectorRef) {
     this.config = config;
 
   }
@@ -50,8 +51,9 @@ export class InicioComponent implements OnInit {
       this.nomeEmpresas.push(cont.nomeDaEmpresa);
     });
 
-    await Promise.all(this.contratos.map(async contrato => {
-      this.saldos = await this.saldoService.getSaldoIndividual(contrato.codigo).toPromise();
+    await Promise.all(this.contratos.map(async (contrato, key) => {
+      this.saldos.push(await this.saldoService.getSaldoIndividual(contrato.codigo).toPromise());
+      console.log(this.saldos[key]);
 
       if (this.saldos.length === 0) {
         this.saldos = null;
@@ -63,14 +65,14 @@ export class InicioComponent implements OnInit {
         let tempDecimo = 0;
         let tempIncidencia = 0;
         let tempMultaFGTS = 0;
-        for (let i = 0; i < this.saldos.length; i++) {
-          tempSaldo = tempSaldo + this.saldos[i].saldo;
-          tempFerias = tempFerias + this.saldos[i].feriasRetido - this.saldos[i].feriasRestituido;
-          tempTerco = tempTerco + this.saldos[i].tercoRetido - this.saldos[i].tercoRestituido;
-          tempDecimo = tempDecimo + this.saldos[i].decimoTerceiroRetido - this.saldos[i].decimoTerceiroRestituido;
-          tempIncidencia = tempIncidencia + (this.saldos[i].incidenciaRetido - this.saldos[i].incidenciaFeriasRestituido -
-            this.saldos[i].incidenciaTercoRestituido - this.saldos[i].incidenciaDecimoTerceiroRestituido);
-          tempMultaFGTS = tempMultaFGTS + this.saldos[i].multaFgtsRetido;
+        for (let i = 0; i < this.saldos[key].length; i++) {
+          tempSaldo = tempSaldo + this.saldos[key][i].saldo;
+          tempFerias = tempFerias + this.saldos[key][i].feriasRetido - this.saldos[key][i].feriasRestituido;
+          tempTerco = tempTerco + this.saldos[key][i].tercoRetido - this.saldos[key][i].tercoRestituido;
+          tempDecimo = tempDecimo + this.saldos[key][i].decimoTerceiroRetido - this.saldos[key][i].decimoTerceiroRestituido;
+          tempIncidencia = tempIncidencia + (this.saldos[key][i].incidenciaRetido - this.saldos[key][i].incidenciaFeriasRestituido -
+            this.saldos[key][i].incidenciaTercoRestituido - this.saldos[key][i].incidenciaDecimoTerceiroRestituido);
+          tempMultaFGTS = tempMultaFGTS + this.saldos[key][i].multaFgtsRetido;
         }
         this.somaSaldo.push(tempSaldo);
         this.somaFerias.push(tempFerias);
@@ -219,18 +221,16 @@ export class InicioComponent implements OnInit {
     this.canvas = document.getElementById('myChart5');
     this.ctx = this.canvas.getContext('2d');
     const color = Chart.helpers.color;
-    let i = -1;
 
     const myChart = new Chart(this.ctx, {
       type: 'radar',
       data: {
         labels: ['Férias', 'Terço constitucional', 'Décimo Terceiro', 'Incidência', 'Multa do FGTS'],
-        datasets: this.nomeEmpresas.map((nomeEmpresa) => {
-          i++;
+        datasets: this.nomeEmpresas.map((nomeEmpresa, key) => {
           const randomColor = this.getRandomColor();
           return {
               label: nomeEmpresa,
-              data: [this.somaFerias[i], this.somaTerco[i], this.somaDecimo[i], this.somaIncidencia[i], this.somaMultaFGTS[i]],
+              data: [this.somaFerias[key], this.somaTerco[key], this.somaDecimo[key], this.somaIncidencia[key], this.somaMultaFGTS[key]],
               backgroundColor: color(randomColor).alpha(0.2).rgbString(),
               borderColor: randomColor,
               pointBackgroundColor: randomColor,
@@ -241,6 +241,13 @@ export class InicioComponent implements OnInit {
       options: {
         legend: {
           position: 'right',
+        },
+        tooltips: {
+          callbacks: {
+            label: (tooltipItem, data) => {
+              return 'Saldo: R$ ' + tooltipItem.yLabel.toFixed(2);
+            }
+          }
         },
       }
     });
@@ -285,34 +292,43 @@ export class InicioComponent implements OnInit {
   }
 
   async montaGraficoPolar() {
-    // console.log(1)
-    // await this.wait()
-    // console.log(2)
     const empresas = [];
-    this.contratos.map((cont) => {
-      empresas.push(cont.nomeDaEmpresa);
+    let temp;
+    const contratos = [];
 
-    });
+    await Promise.all(this.contratos.map(async (cont, key1) => {
+      empresas.push(cont.nomeDaEmpresa);
+      temp = await this.decimoTerceiroService.getRestituicoesDecimoTerceiro(cont.codigo).toPromise();
+      console.log(temp)
+      contratos[key1] = {};
+      await Promise.all(temp.map((valorDec, key) => {
+        const ano = temp[key].inicioContagem.split('-')[0];
+        if (!contratos[key1][ano]) {
+          contratos[key1][ano] = 0;
+        }
+        contratos[key1][ano] = contratos[key1][ano] + temp[key].valoresDecimoTerceiro.valorDecimoTerceiro + temp[key].valoresDecimoTerceiro.valorIncidenciaDecimoTerceiro;
+      }));
+    }));
+    contratos[1][2016] = NaN;
+    console.log(contratos);
     await this.wait();
     this.canvas = document.getElementById('myChart3');
     this.ctx = this.canvas.getContext('2d');
     const myChart = new Chart(this.ctx, {
-      type: 'polarArea',
+      type: 'line',
       data: {
-        labels: empresas,
-        datasets: [{
-          label: '# of Votes',
-          data: this.somaSaldo,
-          backgroundColor: [
-            'rgb(228,82,27,1)',
-            'rgb(255,234,106,1)',
-            'rgb(255,124,16, 1)',
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-          ],
-          borderWidth: 1
-        }]
+        labels: ['2016', '2017', '2018'],
+        datasets: this.nomeEmpresas.map((nomeEmpresa, key) => {
+          const randomColor = this.getRandomColor();
+          return {
+            label: nomeEmpresa,
+            fill: false,
+            data: Object.values(contratos[key]),
+            backgroundColor: randomColor,
+            borderColor: randomColor,
+            borderWidth: 1
+          };
+        })
       },
       options: {
         legend: {
