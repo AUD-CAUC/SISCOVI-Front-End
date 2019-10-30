633cfccb86d9efd5dafbf953ddbc12c689a6ef32
Lucas Penido <lpenidoa@me.com>
2019-10-30 10:54:28 -0300

    Desabilitando campo de início de férias proporcionais caso não haja nenhum período para ser movimentado.

--- src/app/rescisao/calculo-rescisao/movimentacao-rescisao.component.ts
+++ src/app/rescisao/calculo-rescisao/movimentacao-rescisao.component.ts
@@ -69,7 +69,12 @@ export class MovimentacaoRescisaoComponent implements OnInit {
       this.rescisaoForm.get('calcularTerceirizados').get('' + i).get('tipoRescisao');
       this.rescisaoForm.get('calcularTerceirizados').get('' + i).get('tipoRestituicao');
       this.rescisaoForm.get('calcularTerceirizados').get('' + i).get('dataDesligamento');
-      this.rescisaoForm.get('calcularTerceirizados').get('' + i).get('dataInicioFeriasProporcionais').setValue(this.dateToString(this.terceirizados[i].pDataInicioFeriasProporcionais));
+      if (this.dateToString(this.terceirizados[i].pDataInicioFeriasProporcionais)) {
+        this.rescisaoForm.get('calcularTerceirizados').get('' + i).get('dataInicioFeriasProporcionais').setValue(this.dateToString(this.terceirizados[i].pDataInicioFeriasProporcionais));
+      } else {
+        this.rescisaoForm.get('calcularTerceirizados').get('' + i).get('dataInicioFeriasProporcionais').disable();
+        this.rescisaoForm.get('calcularTerceirizados').get('' + i).get('valorFeriasProporcionaisMovimentado').disable();
+      }
       this.rescisaoForm.get('calcularTerceirizados').get('' + i).get('resgateFeriasVencidas').setValidators([Validators.required, this.resgateValidatore]);
       if (!this.terceirizados[i].pDataInicioFeriasIntegrais || !this.terceirizados[i].pDataFimFeriasIntegrais) {
         this.rescisaoForm.get('calcularTerceirizados').get('' + i).get('resgateFeriasVencidas').setValue('N');
